7e6ca6ff397a1ee3900b312e2ab1058bbc175fcf
kenkoba-valks <kenkoba1231@gmail.com>
2020-02-26 15:12:51 +0900

    add the screen of trainers

---
+++ app/helpers/first_view_helper.rb
@@ -0,0 +1,2 @@
+module FirstViewHelper
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
-  root to: 'messages#index'
+  root to: 'first_view#index'
+  resources :first_view, only: [:index]
+  resources :trainer, only: [:index]
 end
