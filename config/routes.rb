a5b00be3ecc87c08f0ca989d4f84b8a311c74e38
kenkoba-valks <kenkoba1231@gmail.com>
2020-02-27 17:08:12 +0900

    install Google map

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   root to: 'first_view#index'
   resources :first_view, only: [:index]
   resources :trainer, only: [:index]
+  resources :messages, only: [:index]
+  resources :location, only: [:index]
 end
