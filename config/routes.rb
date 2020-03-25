6b4b649a4d13f71d13a5f1734fb429e1a2f934d0
kenkoba-valks <kenkoba1231@gmail.com>
2020-03-25 15:54:58 +0900

    install_devise_coach

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,14 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :coaches, controllers: {
+  sessions:      'coaches/sessions',
+  passwords:     'coaches/passwords',
+  registrations: 'coaches/registrations'
+}
+  devise_for :users, controllers: {
+    sessions:      'users/sessions',
+    passwords:     'users/passwords',
+    registrations: 'users/registrations'
+  }
   root "first_view#index"
   resources :first_view, only: [:index]
   resources :location, only: [:index]
